1c74ede3204035bbb30774a004c14347b6f22787
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-01 15:22:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
